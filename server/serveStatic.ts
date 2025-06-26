6a68b23626cca462b714bfa333a81dec1ad7fccb
Houmeecl <151489271+Houmeecl@users.noreply.github.com>
2025-06-26 02:21:56 -0400

    Update serveStatic.ts

--- server/serveStatic.ts
+++ server/serveStatic.ts
@@ -1,21 +1,21 @@
 import express, { type Express } from "express";
+import path from "path";
 import fs from "fs";
-import path, { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.resolve();
 
 export function serveStatic(app: Express) {
-  const distPath = resolve(__dirname, "..", "dist", "public");
+  const distPath = path.resolve(__dirname, "dist", "public");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
-      \`Could not find the build directory: \${distPath}, make sure to build the client first\`
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
     );
   }
 
   app.use(express.static(distPath));
+
   app.use("*", (_req, res) => {
-    res.sendFile(resolve(distPath, "index.html"));
+    res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
